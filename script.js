5e0e5a96343178bf15bd24aa401b36c2e9991f8f
Emmanuel Alawode <alawodeemmanuel2@gmail.com>
2022-06-26 15:58:32 +0100

    Dice code done

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 //=============================================>  # VARIABLE
-let die_value;
+let die_value = 1;
+const die_img = document.getElementsByClassName('die-img');
 const die = document.getElementsByClassName('die');
-
-console.log(die);
 
 //=============================================>  # DIE CODE
 function roll_die() {
   // Math.random generates a random number from 0 to 1
-  die_value = Math.floor(Math.random() * 6);
+  die_value = Math.ceil(Math.random() * 6);
+
+  if (die_value === 1) {
+    die_img[0].src = './assets/die-1.png';
+  }
+
+  if (die_value === 2) {
+    die_img[0].src = './assets/die-2.png';
+  }
+
+  if (die_value === 3) {
+    die_img[0].src = './assets/die-3.png';
+  }
+
+  if (die_value === 4) {
+    die_img[0].src = './assets/die-4.png';
+  }
+
+  if (die_value === 5) {
+    die_img[0].src = './assets/die-5.png';
+  }
+
+  if (die_value === 6) {
+    die_img[0].src = './assets/die-6.png';
+  }
+
+  alert('Player has rolled ' + die_value);
 }
 
-die.onClick();
 //=============================================>  # SCORE BOARD
 
 //=============================================>  # RACE TRACK CODE
