b352dad8fd56e26528e86bdccc4cead826704406
unknown <yomifade@gmail.com>
2022-07-02 13:58:52 +0100

    finished game

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ console.log(die);
 let current_player = 'player_2';
 const player__1_score = document.getElementsByClassName('player__1_score');
 const player__2_score = document.getElementsByClassName('player__2_score');
-
+const player__one_track = document.getElementsByClassName("player__one-track")
+const player__two_track = document.getElementsByClassName("player__two-track")
+const player__one = document.getElementsByClassName("player__one")
+const player__two = document.getElementsByClassName("player__two")
+let player__one_positon = 0
+let player__two_positon = 0
+let winner 
 //=============================================>  # DIE CODE
 function roll_die() {
   // Math.random generates a random number from 0 to 1
@@ -39,6 +45,9 @@ function roll_die() {
   alert('Player has rolled ' + die_value);
   change_player();
   set_score();
+
+  move_car()
+  name_winner()
 }
 
 //=============================================>  # SCORE BOARD
@@ -62,3 +71,26 @@ function set_score() {
   return;
 }
 //=============================================>  # RACE TRACK CODE
+function move_car() {
+  if (current_player === 'player_1') {
+    player__one_positon += die_value
+    player__one[0].style.transform = `translateX(${player__one_positon * 30}px)`
+  }
+  else if(current_player === 'player_2') {
+    player__two_positon += die_value
+    player__two[0].style.transform = `translateX(${player__two_positon * 30}px)`
+  }
+}
+function name_winner() {
+  if (player__one_positon >= 40) {
+  alert ("winer is player one🎉🎉🎉🎈🎈🎈🥳🥳🥳")
+  player__one_positon = 0
+  player__two_positon = 0
+  }
+  else if (player__two_positon >= 40) {
+    alert ("winer is player two🎉🎉🎉🎈🎈🎈🥳🥳🥳")
+    player__one_positon = 0
+    player__two_positon = 0
+    }
+}
+ 
